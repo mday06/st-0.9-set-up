9a8f1301699f1908b2d3e8ac3eb1defbe88f0459
Muhammed <m.ayabakti06@gmail.com>
2022-10-27 18:37:59 +0300

    added back-up of .bashrc

--- config.h
+++ config.h
@@ -99,24 +99,24 @@ float alpha = 0.75;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	[0] = "#282a2e", /* black   */
-	[1] = "#a54242", /* red     */
-	[2] = "#8c9440", /* green   */
-	[3] = "#de935f", /* yellow  */
-	[4] = "#5f819d", /* blue    */
-	[5] = "#85678f", /* magenta */
-	[6] = "#5e8d87", /* cyan    */
-	[7] = "#707880", /* white   */
+	[0] = "#2d2d2d", /* black   */
+	[1] = "#f2777a", /* red     */
+	[2] = "#99cc99", /* green   */
+	[3] = "#ffcc66", /* yellow  */
+	[4] = "#6699cc", /* blue    */
+	[5] = "#cc99cc", /* magenta */
+	[6] = "#66cccc", /* cyan    */
+	[7] = "#d3d0c8", /* white   */
 
 	/* 8 bright colors */
-	[8]  = "#373b41", /* black   */
-	[9]  = "#cc6666", /* red     */
-	[10] = "#b5bd68", /* green   */
-	[11] = "#f0c674", /* yellow  */
-	[12] = "#81a2be", /* blue    */
-	[13] = "#b294bb", /* magenta */
-	[14] = "#8abeb7", /* cyan    */
-	[15] = "#c5c8c6", /* white   */
+	[8]  = "#747369", /* black   */
+	[9]  = "#f2777a", /* red     */
+	[10] = "#99cc99", /* green   */
+	[11] = "#ffcc66", /* yellow  */
+	[12] = "#6699cc", /* blue    */
+	[13] = "#cc99cc", /* magenta */
+	[14] = "#66cccc", /* cyan    */
+	[15] = "#f2f0ec", /* white   */
 
 	/* special colors */
 	[256] = "#1d1f21", /* background */
@@ -130,7 +130,7 @@ static const char *colorname[] = {
 unsigned int defaultfg = 257;
 unsigned int defaultbg = 256;
 unsigned int defaultcs = 257;
-static unsigned int defaultrcs = 15;
+static unsigned int defaultrcs = 7;
 
 /*
  * Default shape of cursor
